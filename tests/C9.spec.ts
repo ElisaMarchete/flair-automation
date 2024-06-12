9777ff89ac5fe5420642784a73d8545a628e9a4c
ElisaMarchete <elisamarchete@gmail.com>
2024-06-11 21:45:12 -0400

    search done wip

--- tests/C9.spec.ts
+++ tests/C9.spec.ts
@@ -2,20 +2,16 @@
  * Feature: Search for a round trip flight with valid details
  */
 
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { addDays, format } from "date-fns";
 
 let today = new Date();
 let day = format(today, "dd");
-let from = format(addDays(new Date(), 1), "dd");
-let to = format(addDays(new Date(), 7), "dd");
-console.log(today);
-console.log(day);
-console.log(from);
-console.log(to);
+let from = format(addDays(new Date(), 1), "EEE MMM dd, yyy"); //Fri Jun 19, 2024
+let to = format(addDays(new Date(), 7), "EEE MMM dd, yyy"); //Fri Jun 19, 2024
 
 // npx playwright codegen https://flyflair.com/
-test("Search for a round trip flight with valid details", async ({page}) => {
+test("Search for a round trip flight with valid details", async ({ page }) => {
   await page.goto("/");
   await page.getByRole("button", { name: "english" }).click();
   await page.getByPlaceholder("from").click();
@@ -24,7 +20,21 @@ test("Search for a round trip flight with valid details", async ({page}) => {
   await page.getByPlaceholder("to").click();
   await page.getByPlaceholder("to").fill("yyc");
   await page.getByText("calgaryyyc").click();
-  await page.getByPlaceholder("depart").click();
-  await page.getByText(from).click();
-  await page.getByText(to).click();
+  page.getByText("departure date", { exact: true });
+  // let calendar = page.getByRole("gridcell");
+  await page.getByLabel(from).click();
+  // await getFrom.click();
+
+  page.getByText("return date", { exact: true });
+  await page.getByLabel(to).click();
+
+  await page.getByRole("button", { name: "done" }).click();
+  await page.getByRole("button", { name: "search flights" }).click();
+
+  // await expect(page.getByTestId(from)).toBeVisible();
+  await expect(page.getByText("choose departing flight")).toBeVisible();
+  await expect(page.locator(`text=${from}`)).toBeVisible();
+
+  await expect(page.getByText("choose return flight")).toBeVisible();
+  await expect(page.locator(`text=${to}`)).toBeVisible();
 });
